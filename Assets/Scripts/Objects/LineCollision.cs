0f38ce084a63fba4d1200e18963e758371e6b987
RenataSaenz <71044498+RenataSaenz@users.noreply.github.com>
2022-10-04 07:11:29 -0300

    parte 1 diamantes

--- Assets/Scripts/Objects/LineCollision.cs
+++ Assets/Scripts/Objects/LineCollision.cs
@@ -7,14 +7,17 @@ using System;
 public class LineCollision : MonoBehaviour
 {
     [SerializeField] EdgeCollider2D _lineCollider;
+    List<Vector2> _colliderList = new List<Vector2>();
     [SerializeField] LineRenderer _lineRenderer;
     [SerializeField] int _count = 0;
     public Action _GrowState = delegate { };
+    [SerializeField] float time;
     void Start()
     {
         _lineRenderer = gameObject.GetComponent<LineRenderer>();
         _lineCollider = gameObject.GetComponent<EdgeCollider2D>();
-       _lineRenderer.SetPosition(0, transform.position);
+        _lineRenderer.SetPosition(0, transform.position);
+        // _colliderList = _lineCollider.GetPoints();
     }
     // Update is called once per frame
     void Update()
@@ -25,6 +28,8 @@ public class LineCollision : MonoBehaviour
     public void AddPoint(int num_arg)
     {
         _lineRenderer.positionCount = num_arg + 1;
+
+        //  _lineCollider.points[].a
     }
 
     bool start = false;
@@ -39,36 +44,42 @@ public class LineCollision : MonoBehaviour
         _GrowState = StartGrow;
 
 
-           // Vector2 distance = nextPos_arg - (Vector2)_lineRenderer.GetPosition(count_arg - 1);
-           //   Vector2 distanceSepareted = distance * 0.2f;
+        // Vector2 distance = nextPos_arg - (Vector2)_lineRenderer.GetPosition(count_arg - 1);
+        //   Vector2 distanceSepareted = distance * 0.2f;
 
 
         //  }
     }
     Vector2 posUpdate;
+    Vector2 posUpdateCollider;
     Vector2 nextPos;
     int count;
     void StartGrow()
     {
-       posUpdate = _lineRenderer.GetPosition(count - 1);
+        posUpdate = _lineRenderer.GetPosition(count - 1);
+        // posUpdateCollider = _lineCollider.points[count - 1];
         AddPoint(count);
         //posUpdate.y = nextPos.y;
         _GrowState = WhileGrow;
     }
     void WhileGrow()
     {
-        if (posUpdate.x < nextPos.x)
-            posUpdate.x += 1 * Time.deltaTime * 8;
+        /*if (posUpdate.x < nextPos.x)
+            posUpdate.x += 1 * Time.deltaTime * time;
+
+        //  _lineCollider.points[count] = nextPos;
 
         if (posUpdate.x > nextPos.x)
-            posUpdate.x -= 1 * Time.deltaTime * 8;
+            posUpdate.x -= 1 * Time.deltaTime * time;
 
         if (posUpdate.y < nextPos.y)
-            posUpdate.y += 1 * Time.deltaTime * 8;
+            posUpdate.y += 1 * Time.deltaTime * time;
 
         if (posUpdate.y > nextPos.y)
-            posUpdate.y -= 1 * Time.deltaTime * 8;
-   
+            posUpdate.y -= 1 * Time.deltaTime * time;*/
+
+        posUpdate = nextPos;
+
         _lineRenderer.SetPosition(0, transform.localPosition);
         _lineRenderer.SetPosition(count, posUpdate);
 
@@ -82,5 +93,6 @@ public class LineCollision : MonoBehaviour
 
 
 
-       
+
+
 }
