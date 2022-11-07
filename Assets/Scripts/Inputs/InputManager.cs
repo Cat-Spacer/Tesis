c09dc684d4d2d35e44b0e288921161c2bada9d9a
Made-In-Madness <90405337+Made-In-Madness@users.noreply.github.com>
2022-11-07 16:05:12 -0300

    Pause menus BGs fix

--- Assets/Scripts/Inputs/InputManager.cs
+++ Assets/Scripts/Inputs/InputManager.cs
@@ -4,60 +4,76 @@ using UnityEngine;
 using System.Linq;
 
 public class InputManager : MonoBehaviour
-{
-    Dictionary<string, KeyCode> buttonKeys;
+{/// <summary>
+ /// Setea las teclas para cada accion
+ /// </summary>
+    //Hacer que dependiendo del voton depende de si es ppal o alternativo
+    private Dictionary<string, KeyCode[]> _buttonKeys;
     private void OnEnable()
     {
-        buttonKeys = new Dictionary<string, KeyCode>();
+        _buttonKeys = new Dictionary<string, KeyCode[]>();
+        // Agarrar del PlayerImputs y cargar las teclas / reemplazarlas (si hubo cambios).
+        _buttonKeys["Jump"] = new KeyCode[2] { KeyCode.Space, KeyCode.LeftControl };
+        _buttonKeys["Left"] = new KeyCode[2] { KeyCode.A, KeyCode.LeftArrow };
+        _buttonKeys["Right"] = new KeyCode[2] { KeyCode.D, KeyCode.RightArrow };
 
-        buttonKeys["Jump"] = KeyCode.Escape;
-        buttonKeys["Left"] = KeyCode.LeftArrow;
-        buttonKeys["Left"] = KeyCode.A;
-        buttonKeys["Right"] = KeyCode.RightArrow;
-        buttonKeys["Right"] = KeyCode.D;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        foreach (var item in _buttonKeys.ToList())
+        {
+            foreach (var value in item.Value)
+            {
+                Debug.Log($"Key = {item.Key} || Value = {value}");
+            }
+        }
     }
 
-    public bool GetButtonDown(string buttonName)
+    public bool GetButtonDown(string buttonName, int index = 0)
     {
-        if (!buttonKeys.ContainsKey(buttonName))
+        if (!_buttonKeys.ContainsKey(buttonName))
         {
             Debug.LogError("Error en nombre del boton");
             return false;
         }
-        return Input.GetKeyDown(buttonKeys[buttonName]);
+        return Input.GetKeyDown(_buttonKeys[buttonName][index]);
     }
 
     public string[] GetButtonNames()
     {
-        return buttonKeys.Keys.ToArray();
+        return _buttonKeys.Keys.ToArray();
     }
 
-    public string GetKeyNamesForButton(string buttonName)
+    public Dictionary<string, KeyCode[]> GetButtonKeys()
+    {
+        return _buttonKeys;
+    }
+
+    public string GetKeyNamesForButton(string buttonName, int index = 0)
     {
 
-        if (!buttonKeys.ContainsKey(buttonName))
+        if (!_buttonKeys.ContainsKey(buttonName))
         {
             Debug.LogError("Error en nombre del boton");
             return "N/A";
         }
 
-        return buttonKeys[buttonName].ToString();
+        return _buttonKeys[buttonName][index].ToString();
     }
 
-    public void SetButtonForKey(string buttonName, KeyCode keyCode)
+    public bool SetButtonForKey(string buttonName, KeyCode keyCode, int alt = 0)
     {
-        buttonKeys[buttonName] = keyCode;
+        if (alt > _buttonKeys.Values.Count())
+            return false;
+
+        foreach (var value in _buttonKeys.Values.ToList())
+        {
+            foreach (var item in value)
+                if (item == keyCode)
+                {
+                    Debug.LogWarning($"The button {keyCode} is alredy used");
+                    return false;
+                }
+        }
+        _buttonKeys[buttonName][alt] = keyCode;
+        return true;
     }
 
 }
