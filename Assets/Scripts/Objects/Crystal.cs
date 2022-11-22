c02a20bab6b12e9f52b60c41355eb0e7b570cebd
Made-In-Madness <90405337+Made-In-Madness@users.noreply.github.com>
2022-11-22 14:38:57 -0300

    Bug fix crystals & level 8 implemented

--- Assets/Scripts/Objects/Crystal.cs
+++ Assets/Scripts/Objects/Crystal.cs
@@ -4,30 +4,26 @@ using UnityEngine;
 
 public class Crystal : MonoBehaviour, IDamageable
 {
-    [SerializeField] private Crystal _nextCrystal;//hacerlo un array
+    [SerializeField] private Crystal _nextCrystal;
     public Crystal _prevCrystal;
     public LineCollision line { get; set; }
     [SerializeField] private bool _forceStart = false, _rotable = true;
     [SerializeField] private int _crystalNumber, _cristalsLenght = 0, _sidesForRotation = 4;
-    [SerializeField] private bool _lastCrystal/*, _usedCrystal = false*/;
+    [SerializeField] private bool _lastCrystal;
     [SerializeField] private GameObject _door;
     public bool _activatedCrystal = false;
-    // [SerializeField] Crystal _nextCrystalIn;
 
     private void Start()
     {
         if (!line)
             line = GetComponent<LineCollision>();
-
-        //_line = FindObjectOfType<LineCollision>();
     }
     private void Update()
     {
-        if (_forceStart/* && !_usedCrystal*/)
+        if (_forceStart)
         {
             CallCrystal(_prevCrystal);
             _forceStart = false;
-            //_usedCrystal = true;
         }
     }
 
@@ -39,32 +35,25 @@ public class Crystal : MonoBehaviour, IDamageable
     {
         if (_lastCrystal && _door != null && _door.GetComponent<Door>())
         {
-            //Debug.Log($"{gameObject.name} last");
             _door.GetComponent<Door>().ActivateDesactivate(!linked);
             line.SetLines(_prevCrystal, _nextCrystal);
             if (!_nextCrystal.line.linkedCrystal)
                 _door.SetActive(true);
         }
-        else
-            Debug.Log($"{gameObject.name} not last");
     }
 
-    //bool called = false;
     public void CallCrystal(Crystal prevCrystal)
     {
-        if (/*called || */prevCrystal == this) return;
+        if (prevCrystal == this) return;
         Debug.Log($"{gameObject.name} call. _prevCrystal = {prevCrystal}");
         _prevCrystal = prevCrystal;
         line.SetLight(_prevCrystal, this, _nextCrystal);
         _activatedCrystal = true;
-        //called = true;
     }
 
     public void RotateCrystal()
     {
-        Debug.Log($"{gameObject.name} rotó {360 / _sidesForRotation} grados");
         transform.Rotate(0, 0, transform.rotation.y + (360 / _sidesForRotation));
-        //called = false;
         if (_activatedCrystal) CallCrystal(_prevCrystal);
     }
 
@@ -85,10 +74,9 @@ public class Crystal : MonoBehaviour, IDamageable
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log($"{gameObject.name} colisiono con {collision.gameObject.name}");
         if (!_prevCrystal) return;
         _prevCrystal.CallCrystal(_prevCrystal._prevCrystal);
         CallCrystal(_prevCrystal);
-        Debug.Log($"{gameObject.name} entre a los calls");
+        if (GetComponent<Rigidbody2D>()) Destroy(GetComponent<Rigidbody2D>());
     }
 }
