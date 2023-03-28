a697a8841e2800f26fbb6c4087b17a408c02185c
unknown <you@example.comsebyliu@2000gmail.com>
2023-03-28 18:46:07 -0300

    Spectrum clone

--- Assets/Scripts/Spectrum/SpectrumInput.cs
+++ Assets/Scripts/Spectrum/SpectrumInput.cs
@@ -2,18 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpectrumInput : MonoBehaviour
+public class SpectrumInput
 {
+    SpectrumBehaviour _spectrum;
     Vector3 screenPosition;
-    public Vector3 targetPosition;
-    private void Update()
+    Vector3 targetPosition;
+
+    public SpectrumInput(SpectrumBehaviour sb)
+    {
+        _spectrum = sb;
+    }
+
+    public void OnUpdate()
+    {
+        Control();
+    }
+    void Control()
     {
         screenPosition = Input.mousePosition;
         targetPosition = Camera.main.ScreenToWorldPoint(screenPosition);
 
-        if (true)
-        {
+        _spectrum.Move(targetPosition);
 
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            _spectrum.Clone();
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            _spectrum.SpectrumMode();
         }
     }
 }
