ff2ef969d21f1bdd7a7c5aa80c7af522218fb777
TituLiu <102271038+TituLiu@users.noreply.github.com>
2023-04-15 16:41:53 -0300

    Pipes

---
+++ Assets/Scripts/Electrical/HamsterInput.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HamsterInput : MonoBehaviour
+{
+    Hamster _hamster;
+    Vector3 screenPosition;
+    Vector3 targetPosition;
+
+    public HamsterInput(Hamster ham)
+    {
+        _hamster = ham;
+    }
+
+    public void OnUpdate()
+    {
+        Control();
+    }
+    void Control()
+    {
+        screenPosition = Input.mousePosition;
+        targetPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            _hamster.GetInTube(targetPosition);
+        }
+    }
+
+}
