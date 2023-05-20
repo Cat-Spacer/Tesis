87c315314971d37fa248e3be622dd9b65af5412b
Made-In-Madness <90405337+Made-In-Madness@users.noreply.github.com>
2023-05-19 23:08:50 -0300

    MetalBox refix

--- Assets/Scripts/Electrical/MetalBox.cs
+++ Assets/Scripts/Electrical/MetalBox.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class MetalBox : MonoBehaviour
 {
-    [SerializeField] private LayerMask _collMask;
+    //[SerializeField] private LayerMask _collMask;
     [SerializeField] private Magnet _magnet;
     private Rigidbody2D _myRB2D = null;
     void Start()
     {
         _myRB2D = GetComponent<Rigidbody2D>();
-        if (_magnet != null)
-            _collMask = _magnet.gameObject.layer;
+        /*if (_magnet != null)
+            _collMask = _magnet.gameObject.layer;*/
     }
 
     private void Update()
     {
         if (_magnet == null) return;
-        if (_magnet.active)
+        if (!_magnet.active)
             _myRB2D.simulated = true;
     }
 
@@ -26,7 +26,7 @@ public class MetalBox : MonoBehaviour
     {
         if (_myRB2D == null || collision.gameObject.GetComponent<Magnet>() == null) return;
         _magnet = collision.gameObject.GetComponent<Magnet>();
-        _collMask = _magnet.gameObject.layer;
+        //_collMask = _magnet.gameObject.layer;
         _myRB2D.simulated = false;
     }
 }
