3228ed619250e5df6ec02d90ee9930404a91b200
Made-In-Madness <90405337+Made-In-Madness@users.noreply.github.com>
2023-05-28 20:34:55 -0300

    Hamster energy system, lavel visfeedback & prefabs fix

---
+++ Assets/Scripts/Player/Hamster/HamsterInput.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HamsterInput : MonoBehaviour
+{
+    Hamster _hamster;
+    Vector3 screenPosition;
+    List<Vector3> targetPosition = new List<Vector3>();
+
+    public HamsterInput(Hamster ham)
+    {
+        _hamster = ham;
+    }
+
+    public void OnUpdate()
+    {
+        Control();
+    }
+
+    void Control()
+    {
+        /*screenPosition = Input.mousePosition;
+        //targetPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+        foreach (var c in Camera.allCameras) if(c.gameObject.activeInHierarchy) targetPosition.Add(c.ScreenToWorldPoint(screenPosition));
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            foreach (var t in targetPosition)
+            {
+                _hamster.GetInTube(t);
+            }
+        }*/
+    }
+}
