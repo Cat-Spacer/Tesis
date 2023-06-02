9e22c543cf67589d466dc892b4661f3f4cdd8728
TituLiu <102271038+TituLiu@users.noreply.github.com>
2023-06-02 17:03:44 -0300

    last fixes

--- Assets/Scripts/IA/HamsterCatch.cs
+++ Assets/Scripts/IA/HamsterCatch.cs
@@ -18,7 +18,7 @@ public class HamsterCatch : MonoBehaviour
                 hamster.MoveToNextTube(hamster.LastTube);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter2D(Collision collision)
     {
         if (!collision.gameObject.GetComponent<Hamster>()) return;
 
