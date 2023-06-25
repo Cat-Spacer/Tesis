72b1f0fbbc7af491f1822ba4b635c527708fb65b
Made-In-Madness <90405337+Made-In-Madness@users.noreply.github.com>
2023-06-25 18:19:13 -0300

    Gral update

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -131,7 +131,8 @@ public class SoundManager : MonoBehaviour
         MagicCat,
         ClestialDiamond,
         StalacticBreaking,
-        MetalFall
+        MetalFall,
+        Button
     }
 
     public void OnClickSound()
