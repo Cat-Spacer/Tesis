e0890de4c4bd71934faa610e4bf1b93443141367
Made-In-Madness <90405337+Made-In-Madness@users.noreply.github.com>
2023-12-23 14:31:17 -0300

    Order Scripts

--- Assets/Scripts/Managers/MenuButton.cs
+++ Assets/Scripts/Managers/MenuButton.cs
@@ -1,7 +1,5 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static UnityEngine.Networking.UnityWebRequest;
 
 public class MenuButton : MonoBehaviour
 {
@@ -9,38 +7,23 @@ public class MenuButton : MonoBehaviour
     [SerializeField] private string _settingsScreenName = "Settings_Menu";
     [SerializeField] private string _controlsScreenName = "Control_Menu";
     private IScreen _activeScreen;
+    
     public void SceneToLoad(int scene)
     {
         AsyncLoadScenes.sceneToLoad = scene;
-        ///AsyncLoadScenes.instance.ChargeAsyncScene(scene);
         SceneManager.LoadScene(loadingScreenIndex);
     }
+
     public void BTN_Settings()
     {
         ScreenManager.Instance.PushInstance(_settingsScreenName);
     }
+
     public void BTN_Controls()
     {
         ScreenManager.Instance.PushInstance(_controlsScreenName);
     }
-    #region Async Scene Load
-    /*
-    public void ChargeAsyncScene(int scene)
-    {
-        var async = SceneManager.LoadSceneAsync(scene);
 
-        StartCoroutine(ChargeCoroutine(async));
-    }
-
-    private IEnumerator ChargeCoroutine(AsyncOperation async)
-    {
-        while (!async.isDone)
-        {
-            yield return new WaitForEndOfFrame();
-        }
-        async.allowSceneActivation = true;
-    }*/
-    #endregion
     public void ExitButton() 
     {
         Application.Quit();
