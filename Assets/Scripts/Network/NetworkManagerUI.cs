f966d5658a0abecffb8d3e07b3ddd4b4efae10ee
unknown <you@example.comsebyliu@2000gmail.com>
2024-05-27 12:31:32 -0300

    Relay connection

--- Assets/Scripts/Network/NetworkManagerUI.cs
+++ Assets/Scripts/Network/NetworkManagerUI.cs
@@ -1,39 +1,178 @@
 using System;
+using System.Linq;
+using System.Net;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine.SceneManagement;
 
 public class NetworkManagerUI : NetworkBehaviour
 {
-    [SerializeField] private Button serverButton;
-    [SerializeField] private Button hostButton;
-    [SerializeField] private Button clientButton;
+    [SerializeField] private GameObject _server;
+    [SerializeField] private GameObject _host;
+    [SerializeField] private GameObject _client;
+    [SerializeField] private Button _serverButton;
+    [SerializeField] private Button _hostButton;
+    [SerializeField] private Button _clientButton;
+    [SerializeField] private Button _backButton;
+    
+    [SerializeField] private TMP_Text _waitingPartner;
+    [SerializeField] private Button _connectClient;
+    [SerializeField] private TMP_InputField _hostCodeText;
+    [SerializeField] private Button _copyCode;
+    [SerializeField] private TMP_InputField _clientCodeText;
+    [SerializeField] private Button _pasteCode;
+
+    private Action _StartGameAction;
     private void Awake()
     {
-        serverButton.onClick.AddListener(() =>
+        _serverButton.onClick.AddListener(() =>
         {
-            NetworkManager.Singleton.StartServer();
+            // _hostButton.gameObject.SetActive(false);
+            // _clientButton.gameObject.SetActive(false);
+            // NetworkManager.Singleton.StartServer();
         });
-        hostButton.onClick.AddListener(() =>
+        _hostButton.onClick.AddListener(() =>
         {
-            NetworkManager.Singleton.StartHost();
+            CreateRelay();
+            HostMenu();
+            _StartGameAction = CheckPlayers;
         });
-        clientButton.onClick.AddListener(() =>
+        _clientButton.onClick.AddListener(ClientMenu);
+        _connectClient.onClick.AddListener(ConnectClient);
+        _backButton.onClick.AddListener(BackButton);
+        _copyCode.onClick.AddListener(CopyCodeBtn);
+        _pasteCode.onClick.AddListener(PasteCodeBtn);
+        _StartGameAction = delegate {  };
+    }
+
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () =>
         {
-            NetworkManager.Singleton.StartClient();
-        });
+            Debug.Log($"Signed in {AuthenticationService.Instance.PlayerId}");
+        };
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
     }
 
     private void Update()
     {
-        if (!IsHost) return;
+        _StartGameAction();
+    }
+    private async void CreateRelay()
+    {
+        try
+        {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(1);
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            _hostCodeText.text = joinCode;
+            RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+            NetworkManager.Singleton.StartHost();
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+    private async void JoinRelay(string joinCode)
+    {
+        try
+        {
+            Debug.Log($"Joining Relay with {joinCode}");
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+            
+            RelayServerData relayServerData = new RelayServerData(joinAllocation, "dtls");
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+            NetworkManager.Singleton.StartClient();
+        }
+        catch(RelayServiceException e){Debug.Log(e);}
+    }
+    void CheckPlayers()
+    {
+        if (!ServerIsHost) return;
+        Debug.Log($"Try start game {NetworkManager.Singleton.ConnectedClients.Count}");
         if (NetworkManager.Singleton.ConnectedClients.Count == 2)
         {
+            Debug.Log("Start game");
             OpenLevelClientRpc();
         }
     }
+    void HostMenu()
+    {
+        _server.gameObject.SetActive(false);
+        _client.gameObject.SetActive(false);
+        _hostButton.gameObject.SetActive(false);
+        _backButton.gameObject.SetActive(true);
+        _waitingPartner.gameObject.SetActive(true);
+    } 
+    void ClientMenu()
+    {
+        _server.gameObject.SetActive(false);
+        _host.gameObject.SetActive(false);
+        _clientButton.gameObject.SetActive(false);
+        _connectClient.gameObject.SetActive(true);
+        _backButton.gameObject.SetActive(true);
+    }
 
+    void MainMenu()
+    {
+        _server.gameObject.SetActive(true);
+        _host.gameObject.SetActive(true);
+        _client.gameObject.SetActive(true);
+        _serverButton.gameObject.SetActive(true);
+        _hostButton.gameObject.SetActive(true);
+        _clientButton.gameObject.SetActive(true);
+        _waitingPartner.gameObject.SetActive(false);
+        _connectClient.gameObject.SetActive(false);
+        _backButton.gameObject.SetActive(false);
+    }
+    void BackButton()
+    {
+        if (ServerIsHost)
+        {
+            Debug.Log("shutdown");
+            _StartGameAction = delegate {  };
+            NetworkManager.Singleton.Shutdown();
+            MainMenu();
+        }
+        else
+        {
+            Debug.Log("to menu");
+            MainMenu();
+        }
+    }
+
+    void CopyCodeBtn()
+    {
+        if (String.IsNullOrEmpty(_hostCodeText.text)) return;
+        TextEditor textEditor = new TextEditor();
+        textEditor.text = _hostCodeText.text;
+        textEditor.SelectAll();
+        textEditor.Copy();
+    }
+    private void PasteCodeBtn()
+    {
+        TextEditor textEditor = new TextEditor();
+        textEditor.multiline = true;
+        textEditor.Paste();
+        _clientCodeText.text = textEditor.text;
+    }
+    void ConnectClient()
+    {
+        JoinRelay(_clientCodeText.text);
+    }
     [ClientRpc]
     void OpenLevelClientRpc()
     {
