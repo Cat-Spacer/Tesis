32456b770b92e0f0b17c1e2a197089e341873418
Made-In-Madness <90405337+Made-In-Madness@users.noreply.github.com>
2024-10-06 17:10:25 -0300

    Turret scripts arrange

--- Assets/Scripts/IA/Factory&Pool/Shoot.cs
+++ Assets/Scripts/IA/Factory&Pool/Shoot.cs
@@ -5,7 +5,7 @@ public class Shoot : MonoBehaviour
     public static void Fire(Bullet bullet, Transform firePoint, GameObject father)
     {
         //Instantiate(bullet, firePoint.position, firePoint.rotation).SetBullet(father);
-        bullet.SetBullet(father);
+        bullet.SetFather(father);
         bullet.gameObject.SetActive(true);
         bullet.transform.position = firePoint.position;
         bullet.transform.rotation = firePoint.rotation;
