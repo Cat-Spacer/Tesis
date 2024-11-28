b464576939e93970528b7317755fa68831e230b6
Sebastian Liu <sebastian.emmanuel.liu@gmail.com>
2024-11-28 18:17:40 -0300

    tutorial bot

---
+++ Assets/Scripts/Tutorial/TutorialBot.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialBot : MonoBehaviour
+{
+    [SerializeField] private float distanceToPlayer;
+    [SerializeField] private float speed;
+    [SerializeField] private float lerpTime;
+    [SerializeField] private Transform target;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        var distance = (transform.position - target.transform.position).magnitude;
+        if (distance > distanceToPlayer)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+
+        if (transform.position.y < target.position.y)
+        {
+            transform.position += speed * Time.deltaTime * Vector3.up;
+        }
+    }
+
+    public void P1ChangeAnimation(TutorialBoxType type)
+    {
+        switch (type)
+        {
+            case TutorialBoxType.None:
+                animator.Play("Happy");
+                break;
+            case TutorialBoxType.Movement:
+                animator.Play("P1MoveKeys");
+                break;
+            case TutorialBoxType.Jump:
+                animator.Play("P1Jump");
+                break;
+            case TutorialBoxType.Interact:
+                animator.Play("P1Interact");
+                break;
+            case TutorialBoxType.Special:
+                animator.Play("P1Special");
+                break;
+            case TutorialBoxType.Punch:
+                animator.Play("P1Punch");
+                break;
+            case TutorialBoxType.DoubleJump:
+                animator.Play("P1DoubleJump");
+                break;
+            case TutorialBoxType.TubesMovement:
+                animator.Play("P1TubesMovement");
+                break;
+        }        
+    }
+    public void P2ChangeAnimation(TutorialBoxType type)
+    {
+        switch (type)
+        {
+            case TutorialBoxType.None:
+                animator.Play("Happy");
+                break;
+            case TutorialBoxType.Movement:
+                animator.Play("P2MoveKeys");
+                break;
+            case TutorialBoxType.Jump:
+                animator.Play("P2Jump");
+                break;
+            case TutorialBoxType.Interact:
+                animator.Play("P2Interact");
+                break;
+            case TutorialBoxType.Special:
+                animator.Play("P2Special");
+                break;
+            case TutorialBoxType.Punch:
+                animator.Play("P2Punch");
+                break;
+            case TutorialBoxType.DoubleJump:
+                animator.Play("P2DoubleJump");
+                break;
+            case TutorialBoxType.TubesMovement:
+                animator.Play("P2TubesMovement");
+                break;
+        }        
+    }
+}
