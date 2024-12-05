a05feaa7e01a2777911282145eb2cdbd0c6218b4
Sebastian Liu <sebastian.emmanuel.liu@gmail.com>
2024-12-05 17:18:18 -0300

    comic

--- Assets/Scripts/Menu/StartMenuIcon.cs
+++ Assets/Scripts/Menu/StartMenuIcon.cs
@@ -8,10 +8,34 @@ public class StartMenuIcon : MonoBehaviour
 {
     private bool canChangeScene;    
     private AsyncOperation asyncLoad;
+    [SerializeField] Animator animator;
+    [SerializeField] Animator skipBtnAnimator;
+    [SerializeField] float accelerationSpeed = 1f;
+    [SerializeField] float decelerationSpeed = 1f;
+    [SerializeField] private float maxAnimationSpeed;
+    [SerializeField] float currentAnimationSpeed;
+    private bool isSkiping = true;
     private void Start()
     {
         LoadSceneAsync("MainMenuCoop");
     }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            currentAnimationSpeed = Mathf.Lerp(currentAnimationSpeed, maxAnimationSpeed, Time.deltaTime * accelerationSpeed);
+        }
+        else
+        {
+            currentAnimationSpeed = Mathf.Lerp(currentAnimationSpeed, 1f, Time.deltaTime * decelerationSpeed);
+        }
+        if(Input.GetKeyDown(KeyCode.Tab)) skipBtnAnimator.Play("SkipButtonPress");
+        if(Input.GetKeyUp(KeyCode.Tab)) skipBtnAnimator.Play("SkipButtonIdle");
+
+        animator.speed = currentAnimationSpeed;
+    }
+
     public void LoadSceneAsync(string sceneName)
     {
         StartCoroutine(LoadSceneCoroutine(sceneName));
