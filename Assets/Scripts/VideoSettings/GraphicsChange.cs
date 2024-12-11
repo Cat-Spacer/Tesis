43b8c5669e99d0904a14cf35ec6c85f4df11bd79
Made-In-Madness <90405337+Made-In-Madness@users.noreply.github.com>
2024-12-11 12:13:46 -0300

    Fixes 11/12/24

--- Assets/Scripts/VideoSettings/GraphicsChange.cs
+++ Assets/Scripts/VideoSettings/GraphicsChange.cs
@@ -2,8 +2,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using System.Linq;
-using System.Security.Cryptography;
 
 
 public class GraphicsChange : MonoBehaviour
@@ -11,6 +9,7 @@ public class GraphicsChange : MonoBehaviour
     [SerializeField] private Toggle _fullscreen = default, _vsync = default;
     [SerializeField] private int _resIndex = 0, _aaIndex = 0;
     [SerializeField] private TMP_Dropdown _qualityDropdown = default, _resolutionDropdown = default;
+    [SerializeField] private Resolution[] _alowedResolutions = default;
 
     Resolution[] _resolutions = default;
 
@@ -21,12 +20,10 @@ public class GraphicsChange : MonoBehaviour
 
     private void Setting()
     {
-
         if (QualitySettings.vSyncCount == 0)
             _vsync.isOn = false;
         else
             _vsync.isOn = true;
-
 
 
         _fullscreen.isOn = Screen.fullScreen;
@@ -34,18 +31,19 @@ public class GraphicsChange : MonoBehaviour
         _resolutions = Screen.resolutions;
         _resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
-        
+
         _resIndex = 0;
-        
+
         for (int i = 0; i < _resolutions.Length; i++)
         {
-            string option = _resolutions[i].width + " x " + _resolutions[i].height + " " + Mathf.RoundToInt(_resolutions[i].refreshRateRatio.numerator) + "Hz";
+            if (Mathf.RoundToInt(_resolutions[i].refreshRateRatio.numerator) / 10 == 60 && (_resolutions[i].width % 16 == 0 && _resolutions[i].height % 9 == 0)) continue;
+            string option = $"{_resolutions[i].width} x {_resolutions[i].height}";
             options.Add(option);
-            if (_resolutions[i].width == Screen.currentResolution.width && _resolutions[i].height == Screen.currentResolution.height)
-            {
+            if (_resolutions[i].width == Screen.currentResolution.width &&
+                _resolutions[i].height == Screen.currentResolution.height)
                 _resIndex = i;
-            }
         }
+
         _resolutionDropdown.AddOptions(options);
         _resolutionDropdown.value = _resIndex;
         _resolutionDropdown.RefreshShownValue();
@@ -61,15 +59,17 @@ public class GraphicsChange : MonoBehaviour
     {
         _resIndex = _resolutionDropdown.value;
     }
+
     public void SetAntiAliasing(int aaIndex)
     {
         QualitySettings.antiAliasing = aaIndex;
         if (_qualityDropdown) _qualityDropdown.value = 6;
     }
+
     public void ApplyGraphics()
     {
         Screen.fullScreen = _fullscreen.isOn;
-        
+
         if (_vsync.isOn)
             QualitySettings.vSyncCount = 1;
         else
