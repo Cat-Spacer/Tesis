d9062006f4c9b5a4a364da33a54c7752fea26e5d
Sebastian Liu <sebastian.emmanuel.liu@gmail.com>
2024-12-10 23:04:03 -0300

    color

---
+++ Assets/Scripts/Enums/InteractionColorsEnum.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InteractionColorsEnum
+{
+    Orange,
+    Yellow,
+    BlackCyan,
+    Pink,
+    Grey
+}
